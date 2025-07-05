b24cc5560a88fb849218ae1de0d823bcddf8b723
emergomes2 <emergomes2@gmail.com>
2025-07-04 22:12:33 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ControlCenter from "./ControlCenter";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ControlCenter />
+  </React.StrictMode>
+);
