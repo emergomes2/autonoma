bb08e1474e19924882a65e346956885356d67b50
emergomes2 <emergomes2@gmail.com>
2025-07-04 21:15:21 -0300

    Create ControlCenter.jsx

---
+++ src/ControlCenter.jsx
@@ -0,0 +1,8 @@
+// src/ControlCenter.jsx
+export default function ControlCenter() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
+      <h1>Autonoma está no ar, Emmer!</h1>
+    </div>
+  );
+}
