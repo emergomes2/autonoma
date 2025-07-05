c36d22eed1ffa917bfdc72d9b1adf769d72e734f
emergomes2 <emergomes2@gmail.com>
2025-07-04 22:20:54 -0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,11 +1,9 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ControlCenter from "./ControlCenter";
-import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ControlCenter />
+    <App />
   </React.StrictMode>
 );
